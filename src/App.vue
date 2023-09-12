e505ef4e23c967311e2ab5f997f26afa79895e01
NatáliaFonseca <fonsecanatalianf@gmail.com>
2023-09-12 14:23:54 -0300

    feat: creating a calculator

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<script setup>
+  import { reactive } from 'vue';
+  import Cabecalho from './components/Cabecalho.vue'
+  import Formulario from './components/Formulario.vue'
+  import Resultado from './components/Resultado.vue'
+
+const estado= reactive({
+primeiroNumero:" ",
+segundoNumero:" ",
+resultado:" ",
+
+})
+
+
+function realizarOperacao(operacao){
+switch(operacao){
+case 'somar':
+estado.resultado = estado.primeiroNumero + estado.segundoNumero;
+break
+case 'subtrair':
+estado.resultado = estado.primeiroNumero - estado.segundoNumero;
+break
+case 'multiplicar':
+estado.resultado = estado.primeiroNumero * estado.segundoNumero;
+break
+case 'dividir':
+estado.resultado = estado.primeiroNumero / estado.segundoNumero;
+break
+default:
+estado.resultado=0;
+}
+}
+</script>
+
+<template>
+
+<div class="container-md bg-info-subtle"  > 
+  <Cabecalho/>
+<Formulario @realizar-operacao="realizarOperacao" :primeiro-numero="estado.primeiroNumero" :segundo-numero="estado.segundoNumero"/>
+<Resultado :resultado="estado.resultado"/>
+</div>
+</template>
+
+
+<style scoped>
+
+
+
+</style>
---
+++ src/components/Resultado.vue
@@ -0,0 +1,8 @@
+<script setup>
+  const props = defineProps([ 'resultado'])
+</script>
+
+
+<template>
+  <h1 class="text-center m-2">{{props.resultado}}</h1>
+</template>
